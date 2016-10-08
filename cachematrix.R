4544c1f7b8748ccff0dbc8c218b1eff5def8d3bf
Alla E <aeizenbe@hotmail.com>
2016-10-08 15:37:59 -0400

    My changes to cacematrix code

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## return: a list containing functions to
+        ##              1. set the matrix
+        ##              2. get the matrix
+        ##              3. set the inverse
+        ##              4. get the inverse
+        ##         this list is used as the input to cacheSolve()
+        
+        
+        inv = NULL
+        set = function(y) {
+        # use `<<-` to assign a value to an object in an environment 
+        # different from the current environment. 
+                x <<- y
+                inv <<- NULL
+        }
+        get = function() x
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
@@ -12,4 +31,43 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## return: inverse of the original matrix input to makeCacheMatrix()
+        
+        inv = x$getinv()
+        
+        # if the inverse has already been calculated
+        if (!is.null(inv)){
+                # get it from the cache and skips the computation. 
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # otherwise, calculates the inverse 
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        # sets the value of the inverse in the cache via the setinv function.
+        x$setinv(inv)
+        
+        return(inv)
+        
 }
+
+my_test = function(my_matrix){
+ 
+  print("My original matrix")
+  print(my_matrix)
+  
+  cacheMatrix = makeCacheMatrix(my_matrix)
+  
+  
+  inversionMatrix = cacheSolve(cacheMatrix)
+  print("Inversion matrix before cache is:")
+  print(inversionMatrix)
+  
+  inversionMatrix = cacheSolve(cacheMatrix)
+  print("Inversion matrix from cache is:")
+  print(inversionMatrix)
+  
+}
+
